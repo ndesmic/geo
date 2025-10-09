1b2a427e138180685c4aef7b3a1311d3d010098a
ndesmic <ndesmic@duck.com>
2025-10-08 22:23:46 -0700

    update diagnostic test

--- visual-tests/diagnostic.spec.js
+++ visual-tests/diagnostic.spec.js
@@ -4,7 +4,7 @@ test("Environment should be setup correctly", async ({ page }) => {
     page.goto("chrome://gpu");
     await expect(page).toHaveTitle("GPU Internals");
     await page.evaluate(async () => {
-        document.querySelector("info-view").shadowRoot.querySelector(".info-table:first-child tr:first-of-type").style.display = "none";
+        document.querySelector("info-view").shadowRoot.querySelectorAll("#content > div:not(:first-of-type)").forEach(x => x.style.display = "none");
     });
     await expect(page.getByText('WebGPU: Hardware accelerated')).toBeVisible();
     await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.05 });
